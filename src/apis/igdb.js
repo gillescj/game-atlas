7430dd9347dbccb2fa7d33aa87358d9fa7772690
Colin Gillespie <ratchetandcolin@gmail.com>
2020-05-06 15:56:48 -0400

    Swapped to more reliable cors proxy

--- src/apis/igdb.js
+++ src/apis/igdb.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 const KEY = 'efb5f487c944d641ebb3d0394fafc386';
 
 export default axios.create({
-    baseURL: 'https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/',
+    baseURL: 'https://gillescj-cors-anywhere.herokuapp.com/https://api-v3.igdb.com/',
     timeout: 0,
     headers: {
         Accept: 'application/json',
         'user-key': KEY,
-        'Content-Type': 'text/plain'
-    }
+        'Content-Type': 'text/plain',
+    },
 });
